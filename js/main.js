60f6def7f1eba790736c476599f03ae41d31a199
sophwest <s_westrop2@fanshaweonline.ca>
2023-02-17 10:25:17 -0500

    playing audio on a click

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-console.log("JS is running...");
+console.log('JS is running...');
+
+// select all the placeholder images
+let albumCovers = document.querySelectorAll('.audio img');
+
+
+//play the audio
+function playTrack() {
+    // debugger;
+    // find the right audio track and play it based on the dataset attribute
+    let currentAudio = document.querySelector(`audio[data-trackref="${this.dataset.trackref}"]`);
+    currentAudio.play();
+}
+
+
+// add the reactions (the event we want to listen for)
+albumCovers.forEach(album => album.addEventListener('click', playTrack));
