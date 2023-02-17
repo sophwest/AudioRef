ebc5543d19f3c4ed59fad63ebc4adc1d311c97b3
sophwest <s_westrop2@fanshaweonline.ca>
2023-02-17 11:15:22 -0500

    added controls

--- js/main.js
+++ js/main.js
@@ -1,17 +1,39 @@
 console.log('JS is running...');
 
 // select all the placeholder images
-let albumCovers = document.querySelectorAll('.audio img');
+let albumCovers = document.querySelectorAll('.audio img'),
+audioEl = document.querySelector('audio'),
+playButton = document.querySelector('#playButton');
+pauseButton = document.querySelector('#pauseButton');
+rewindButton = document.querySelector('#rewindButton');
 
+function loadAudio() {
+// find the right audio track and play it based on the dataset attribute
+audioEl.src = `audio/${this.dataset.trackref}.mp3`;
+audioEl.load();
+
+//now i can play audio without things breaking
+playTrack();
+}
 
 //play the audio
 function playTrack() {
-    // debugger;
-    // find the right audio track and play it based on the dataset attribute
-    let currentAudio = document.querySelector(`audio[data-trackref="${this.dataset.trackref}"]`);
-    currentAudio.play();
+    audioEl.play();
+}
+
+function pauseTrack() {
+    audioEl.pause();
+}
+
+function rewindTrack() {
+    audioEl.currentTime = 0
 }
 
 
 // add the reactions (the event we want to listen for)
 albumCovers.forEach(album => album.addEventListener('click', playTrack));
+
+//custom audio controls
+playButton.addEventListener('click', playTrack);
+pauseButton.addEventListener('click', pauseTrack);
+rewindButton.addEventListener('click', rewindTrack);
